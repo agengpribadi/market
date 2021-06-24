d8e395a38fa8848e319d68677ef650c713b78ec6
ageng pribadi <ageng.pribadi@difinite.com>
2021-06-24 17:36:05 +0700

    try again

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,21 +1,17 @@
-const path = require('path');
-const {Externals} = require('share-loader');
 module.exports = {
-  externals: [
-    Externals({
-      namespace: 'container-app',
-      modules: [/@angular/]
-    })
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-    chunkFilename: '[name].js',
-    library: 'extapp',
-    libraryTarget: 'umd'
-  },
-  optimization: {
-    runtimeChunk: false,
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use:
+          [{
+            loader: 'share-loader',
+            options: {
+              modules: [/@angular/],
+              exclude: [/@angular-devkit/],
+              namespace: 'container-app'
+            }
+          }]
+      }]
   }
-
-}
+};
